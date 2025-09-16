7f3e921b63659f9463da1c0c517322710f6cf83c
Jan Mottl <jan.mottl@ackee.cz>
2025-09-16 10:30:07 +0200

    Fix POM developer email

--- build-logic/logic/src/main/kotlin/io/github/ackeecz/ackeelities/plugin/PublishingPlugin.kt
+++ build-logic/logic/src/main/kotlin/io/github/ackeecz/ackeelities/plugin/PublishingPlugin.kt
@@ -51,7 +51,7 @@ internal class PublishingPlugin : Plugin<Project> {
                     developer {
                         id.set(libraryProperties.pomDeveloperId)
                         name.set(libraryProperties.pomDeveloperName)
-                        url.set(libraryProperties.pomDeveloperEmail)
+                        email.set(libraryProperties.pomDeveloperEmail)
                     }
                 }
                 scm {
